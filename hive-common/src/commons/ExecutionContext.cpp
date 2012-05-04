95008ca8be16175a891e718467797a33b63c0552
Rafał Lewandowski <rlewano@gmail.com>
2012-05-04 14:35:40 +0000

    Added the possibility to allocate and deallocate memory buffers on the device.
    git-svn-id: https://kask.eti.pg.gda.pl/svn/kernelhive/KernelHive/src@37 1473d755-6451-4e3e-83a9-0691c74848a3

--- hive-common/src/commons/ExecutionContext.cpp
+++ hive-common/src/commons/ExecutionContext.cpp
@@ -14,11 +14,39 @@ namespace KernelHive {
 	}
 
 	ExecutionContext::~ExecutionContext() {
+		releaseBuffers();
 		if (clCommandQueue != NULL) {
+			clFlush(clCommandQueue);
+			clFinish(clCommandQueue);
 			clReleaseCommandQueue(clCommandQueue);
 		}
 		if (clContext != NULL) {
 			clReleaseContext(clContext);
+		}
+	}
+
+	void ExecutionContext::createBuffer(const std::string& name, size_t size, cl_mem_flags flags) {
+		cl_int errorCode;
+		cl_mem buffer = clCreateBuffer(clContext, flags, size, NULL, &errorCode);
+		if (errorCode == CL_SUCCESS) {
+			buffers[name] = buffer;
+		} else {
+			std::string message = "Error creating an OpenCL buffer object";
+			throw OpenClException(message, errorCode);
+		}
+	}
+
+	void ExecutionContext::releaseBuffer(const std::string& name) {
+		cl_int errorCode;
+		BufferMap::iterator iterator = buffers.find(name);
+		if (iterator != buffers.end()) {
+			errorCode = clReleaseMemObject(buffers[name]);
+			if ( errorCode == CL_SUCCESS) {
+				buffers.erase(name);
+			} else {
+				std::string message = "Error releasing an OpenCL buffer object";
+				throw OpenClException(message, errorCode);
+			}
 		}
 	}
 
@@ -29,7 +57,7 @@ namespace KernelHive {
 	void ExecutionContext::initOpenClContext() {
 		const cl_context_properties properties[] = {
 				CL_CONTEXT_PLATFORM,
-				(cl_context_properties) device.getClDeviceId(),
+				(cl_context_properties) device.getClPlatformId(),
 				0
 		};
 		cl_int errorCode;
@@ -48,6 +76,16 @@ namespace KernelHive {
 		if (errorCode != CL_SUCCESS) {
 			std::string message = "Error initializing OpenCL command queue";
 			throw OpenClException(message, errorCode);
+		}
+	}
+
+	void ExecutionContext::releaseBuffers() {
+		if (buffers.size() > 0) {
+			BufferMap::iterator iterator;
+			for (iterator = buffers.begin(); iterator != buffers.end(); iterator++) {
+				clReleaseMemObject(iterator->second);
+			}
+			buffers.clear();
 		}
 	}
 
--- hive-common/src/commons/ExecutionContext.h
+++ hive-common/src/commons/ExecutionContext.h
@@ -2,10 +2,14 @@
 #define KERNEL_HIVE_EXECUTION_CONTEXT_H
 
 #include <CL/cl.h>
+#include <map>
 
 #include "OpenClDevice.h"
 
 namespace KernelHive {
+
+	/** The type to use for the meomry buffers. */
+	typedef std::map<std::string, cl_mem> BufferMap;
 
 	/**
 	 * Represents an execution context in which a kernel runs.
@@ -29,6 +33,23 @@ namespace KernelHive {
 		 */
 		virtual ~ExecutionContext();
 
+		/**
+		 * Creates a new device memory buffer. The buffer can be later
+		 * referenced by the provided name.
+		 *
+		 * @param name the name ro use for the buffer
+		 * @param size the size of the new buffer
+		 * @param buffer OpenCL flags to use when creating this buffer
+		 */
+		void createBuffer(const std::string& name, size_t size, cl_mem_flags flags);
+
+		/**
+		 * Releases a buffer with the provided name.
+		 *
+		 * @param name the name of the buffer to release
+		 */
+		void releaseBuffer(const std::string& name);
+
 	private:
 		/** The OpenCL device to use for this context. */
 		OpenClDevice device;
@@ -39,12 +60,16 @@ namespace KernelHive {
 		/** The OpenCL command queue. */
 		cl_command_queue clCommandQueue;
 
+		/** A map which holds all buffers used in this execution context. */
+		BufferMap buffers;
+
 		/** Initializes the OpenCL context. */
 		void initOpenClContext();
 
 		/** Initializes an OpenCL command queue. */
 		void initOpenClCommandQueue();
 
+		void releaseBuffers();
 	};
 
 } /* namespace KernelHive */
