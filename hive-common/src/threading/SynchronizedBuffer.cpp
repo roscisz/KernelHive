ebbe2c890aca44ca3314368bf8f62d3370337f0b
Rafał Lewandowski <rlewano@gmail.com>
2012-05-21 18:07:42 +0000

    Implemented synced buffer methods.
    git-svn-id: https://kask.eti.pg.gda.pl/svn/kernelhive/KernelHive/src@60 1473d755-6451-4e3e-83a9-0691c74848a3

--- hive-common/src/threading/SynchronizedBuffer.cpp
+++ hive-common/src/threading/SynchronizedBuffer.cpp
@@ -2,12 +2,69 @@
 
 namespace KernelHive {
 
+// ========================================================================= //
+// 							Public Members									 //
+// ========================================================================= //
+
 SynchronizedBuffer::SynchronizedBuffer() {
-	// TODO Auto-generated constructor stub
+	init();
+}
+
+SynchronizedBuffer::SynchronizedBuffer(size_t size) {
+	init();
+	allocateInternal(size);
 }
 
 SynchronizedBuffer::~SynchronizedBuffer() {
-	// TODO Auto-generated destructor stub
+	releaseResources();
+}
+
+void SynchronizedBuffer::allocate(size_t size) {
+	pthread_mutex_lock(&bufferLock);
+	allocateInternal(size);
+	pthread_mutex_unlock(&bufferLock);
+}
+
+void SynchronizedBuffer::append(byte* data, size_t amount) {
+	pthread_mutex_lock(&bufferLock);
+	for (unsigned int i = 0; i < amount; i++) {
+		this->data[position] = data[i];
+		position++;
+	}
+	pthread_mutex_unlock(&bufferLock);
+}
+
+size_t SynchronizedBuffer::getSize() {
+	return size;
+}
+
+byte* SynchronizedBuffer::getRawData() {
+	return data;
+}
+
+// ========================================================================= //
+// 							Private Members									 //
+// ========================================================================= //
+
+void SynchronizedBuffer::init() {
+	size = 0;
+	position = 0;
+	data = NULL;
+	pthread_mutex_init(&bufferLock, NULL);
+}
+
+void SynchronizedBuffer::allocateInternal(size_t size) {
+	releaseResources();
+	data = new byte[size];
+	this->size = size;
+}
+
+void SynchronizedBuffer::releaseResources() {
+	if (data != NULL) {
+		delete [] data;
+	}
+	size = 0;
+	position = 0;
 }
 
 } /* namespace KernelHive */
--- hive-common/src/threading/SynchronizedBuffer.h
+++ hive-common/src/threading/SynchronizedBuffer.h
@@ -1,12 +1,99 @@
 #ifndef KERNEL_HIVE_SYNCHRONIZED_BUFFER_H
 #define KERNEL_HIVE_SYNCHRONIZED_BUFFER_H
 
+#include <cstdlib>
+#include <pthread.h>
+
 namespace KernelHive {
 
+/** A data type alias. */
+typedef char byte;
+
+/**
+ * A thread-safe buffer.
+ */
 class SynchronizedBuffer {
+
 public:
+	/**
+	 * Initializes this synchronized buffer.
+	 */
 	SynchronizedBuffer();
+
+	/**
+	 * Initializes this buffer with provided size.
+	 *
+	 * @param size the number of bytes this buffer should allocate
+	 */
+	SynchronizedBuffer(size_t size);
+
+	/**
+	 * Deallocates resources used by this buffer.
+	 */
 	virtual ~SynchronizedBuffer();
+
+	/**
+	 * Allocates this buffer with provided size. Any data previously
+	 * allocated and written will be released.
+	 *
+	 * @param size the size of the new buffer
+	 */
+	void allocate(size_t size);
+
+	/**
+	 * Returns the amount of bytes allocated in this buffer.
+	 *
+	 * @return the amount of bytes allocated in this buffer
+	 */
+	size_t getSize();
+
+	/**
+	 * Appends provided data to the end of this buffer.
+	 *
+	 * @param data the data to append
+	 * @param amount the number of bytes to append
+	 */
+	void append(byte* data, size_t amount);
+
+	/**
+	 * Returns a pointer to data allocated in this buffer. This is the pointer
+	 * to the internal buffer, so it's contents may be subject to overwriting
+	 * by any other threads using it!
+	 *
+	 * @return the pointer to buffer's data
+	 */
+	byte* getRawData();
+
+private:
+	/** The number of bytes allocated by this buffer. */
+	size_t size;
+
+	/** The position pointer of this buffer. */
+	unsigned int position;
+
+	/** The data stored in this buffer. */
+	byte* data;
+
+	/** The lock object used to synchronize this buffer. */
+	pthread_mutex_t bufferLock;
+
+	/**
+	 * Initializes this buffer.
+	 */
+	void init();
+
+	/**
+	 * Performs the actual allocation.
+	 *
+	 * @param size the number of bytes to allocate
+	 */
+	void allocateInternal(size_t size);
+
+	/**
+	 * Releases any resources stored in this buffer
+	 */
+	void releaseResources();
+
 };
 
 } /* namespace KernelHive */
