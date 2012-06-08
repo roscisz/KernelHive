f15be07d91fbeabc6e32fe337a57067cbc2add59
Paweł Rościszewski <pawel.rosciszewski@pg.edu.pl>
2012-06-08 15:57:18 +0000

    Added ant build script and service start script for hive-cluster.
    git-svn-id: https://kask.eti.pg.gda.pl/svn/kernelhive/KernelHive/src@100 1473d755-6451-4e3e-83a9-0691c74848a3

--- hive-cluster/src/org/kernelhive/cluster/HiveCluster.java
+++ hive-cluster/src/org/kernelhive/cluster/HiveCluster.java
@@ -19,18 +19,17 @@ public class HiveCluster implements Daemon {
 
 	@Override
 	public void destroy() {
-		// TODO Auto-generated method stub
-		
+		System.out.println("SVC DESTROY");		
 	}
 
 	@Override
 	public void init(DaemonContext arg0) throws DaemonInitException, Exception {
-		// TODO Auto-generated method stub
-		
+		System.out.println("SVC INIT");
 	}
 
 	@Override
 	public void start() throws Exception {
+		System.out.println("SVC START");
 		ClusterManager cm = new ClusterManager();	
 		
 		// TODO: connect to the execution engine
@@ -40,8 +39,7 @@ public class HiveCluster implements Daemon {
 
 	@Override
 	public void stop() throws Exception {
-		// TODO Auto-generated method stub
-		
+		System.out.println("SVC STOP");
 	}
 
 }
