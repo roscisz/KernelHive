8135eca96e751a9da6ed67267a7a7e5dc256a978
Marcel Schally-Kacprzak <marcel.schally@gmail.com>
2012-07-10 18:42:42 +0000

    git-svn-id: https://kask.eti.pg.gda.pl/svn/kernelhive/KernelHive/src@141 1473d755-6451-4e3e-83a9-0691c74848a3

---
+++ hive-jcommon/trunk/test/pl/gda/pg/eti/kernelhive/common/graph/configuration/impl/GraphConfigurationTest.java
@@ -0,0 +1,34 @@
+package pl.gda.pg.eti.kernelhive.common.graph.configuration.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class GraphConfigurationTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testGetProjectName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSaveGraphListOfIGraphNode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLoadGraph() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetConfigurationFile() {
+		fail("Not yet implemented");
+	}
+
+}
