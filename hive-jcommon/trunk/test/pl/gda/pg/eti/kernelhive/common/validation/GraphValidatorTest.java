be2bc4b66acbeb60ea1f34845c2afc8711857b3e
Marcel Schally-Kacprzak <marcel.schally@gmail.com>
2012-07-16 17:28:07 +0000

    git-svn-id: https://kask.eti.pg.gda.pl/svn/kernelhive/KernelHive/src@157 1473d755-6451-4e3e-83a9-0691c74848a3

--- hive-jcommon/trunk/test/pl/gda/pg/eti/kernelhive/common/validation/GraphValidatorTest.java
+++ hive-jcommon/trunk/test/pl/gda/pg/eti/kernelhive/common/validation/GraphValidatorTest.java
@@ -8,8 +8,6 @@ import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 
-import pl.gda.pg.eti.kernelhive.common.graph.factory.IGraphNodeFactory;
-import pl.gda.pg.eti.kernelhive.common.graph.factory.impl.GraphNodeFactory;
 import pl.gda.pg.eti.kernelhive.common.graph.node.GraphNodeType;
 import pl.gda.pg.eti.kernelhive.common.graph.node.IGraphNode;
 import pl.gda.pg.eti.kernelhive.common.graph.node.util.NodeIdGenerator;
@@ -23,23 +21,24 @@ public class GraphValidatorTest {
 	@Before
 	public void setUp() throws Exception {
 		projectNodes = new ArrayList<IGraphNode>();
-		IGraphNodeFactory factory = new GraphNodeFactory();
-		IGraphNode node1 = factory.createGraphNode(GraphNodeType.GENERIC);
-		node1.setNodeId(NodeIdGenerator.generateId());
-		IGraphNode node2 = factory.createGraphNode(GraphNodeType.GENERIC);
-		node2.setNodeId(NodeIdGenerator.generateId());
-		projectNodes.add(node1);
-		projectNodes.add(node2);
+//		IGraphNodeFactory factory = new GraphNodeFactory();
+//		IGraphNode node1 = factory.createGraphNode(GraphNodeType.GENERIC);
+//		node1.setNodeId(NodeIdGenerator.generateId());
+//		IGraphNode node2 = factory.createGraphNode(GraphNodeType.GENERIC);
+//		node2.setNodeId(NodeIdGenerator.generateId());
+//		projectNodes.add(node1);
+//		projectNodes.add(node2);
 		
 	}
 
 	@Test
 	public void testValidateGraphPass() {
-		projectNodes.get(0).addFollowingNode(projectNodes.get(1));
-		List<ValidationResult> list = GraphValidator.validateGraph(projectNodes);
-		for(ValidationResult res : list){
-			assertEquals(res.getMesssage(), ValidationResultType.VALID, res.getType());
-		}
+//		projectNodes.get(0).addFollowingNode(projectNodes.get(1));
+//		List<ValidationResult> list = GraphValidator.validateGraph(projectNodes);
+//		for(ValidationResult res : list){
+//			assertEquals(res.getMesssage(), ValidationResultType.VALID, res.getType());
+//		}
+		fail("");
 	}
 	
 	@Test
