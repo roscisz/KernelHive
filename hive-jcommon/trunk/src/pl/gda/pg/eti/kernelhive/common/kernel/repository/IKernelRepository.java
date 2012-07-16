3c1ea20dc04513ba55d9f9df5b77ebb91d3eff68
Marcel Schally-Kacprzak <marcel.schally@gmail.com>
2012-07-16 17:25:30 +0000

    git-svn-id: https://kask.eti.pg.gda.pl/svn/kernelhive/KernelHive/src@156 1473d755-6451-4e3e-83a9-0691c74848a3

--- hive-jcommon/trunk/src/pl/gda/pg/eti/kernelhive/common/kernel/repository/IKernelRepository.java
+++ hive-jcommon/trunk/src/pl/gda/pg/eti/kernelhive/common/kernel/repository/IKernelRepository.java
@@ -14,14 +14,14 @@ import pl.gda.pg.eti.kernelhive.common.graph.node.GraphNodeType;
 public interface IKernelRepository {
 	
 	/**
-	 * 
+	 * gets {@link KernelRepositoryEntry} for the given {@link GraphNodeType}
 	 * @param type {@link GraphNodeType}
 	 * @return {@link KernelRepositoryEntry}
 	 * @throws ConfigurationException
 	 */
 	KernelRepositoryEntry getEntryForGraphNodeType(GraphNodeType type) throws ConfigurationException;
 	/**
-	 * 
+	 * gets a {@link List} of {@link KernelRepositoryEntry} objects
 	 * @return list of {@link KernelRepositoryEntry} 
 	 * @throws ConfigurationException
 	 */
