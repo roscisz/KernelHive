725c9bd52d685514fd662ea0a88a72ddb9b86ee7
Paweł Rościszewski <pawel.rosciszewski@pg.edu.pl>
2012-07-23 17:47:54 +0000

    Updatet WS client import script.
    git-svn-id: https://kask.eti.pg.gda.pl/svn/kernelhive/KernelHive/src@173 1473d755-6451-4e3e-83a9-0691c74848a3

--- hive-jcommon/wsimport.sh
+++ hive-jcommon/wsimport.sh
@@ -1,14 +1,24 @@
 TMPDIR=wsimport
-WSDL=http://localhost:8080/ClusterBeanService/ClusterBean?wsdl
-PACKAGE=pl.gda.pg.eti.kernelhive.common.structure
-PKGDIR=pl/gda/pg/eti/kernelhive/common/structure
+CLUSTER_WSDL=http://localhost:8080/ClusterBeanService/ClusterBean?wsdl
+CLUSTER_PACKAGE=pl.gda.pg.eti.kernelhive.common.clusterService
+CLUSTER_PKGDIR=pl/gda/pg/eti/kernelhive/common/clusterService
+CLIENT_WSDL=http://localhost:8080/ClientBeanService/ClientBean?wsdl
+CLIENT_PACKAGE=pl.gda.pg.eti.kernelhive.common.clientService
+CLIENT_PKGDIR=pl/gda/pg/eti/kernelhive/common/clientService
 DESTDIR=trunk/src/
 
 mkdir $TMPDIR
-wsimport -verbose -s $TMPDIR -p $PACKAGE -keep -Xnocompile $WSDL 
-cp $TMPDIR/$PKGDIR/ClusterBean.java $DESTDIR/$PKGDIR
-cp $TMPDIR/$PKGDIR/ClusterBeanService.java $DESTDIR/$PKGDIR
-cp $TMPDIR/$PKGDIR/ObjectFactory.java $DESTDIR/$PKGDIR
-cp $TMPDIR/$PKGDIR/Update.java $DESTDIR/$PKGDIR
-cp $TMPDIR/$PKGDIR/UpdateResponse.java $DESTDIR/$PKGDIR
+wsimport -verbose -s $TMPDIR -p $CLUSTER_PACKAGE -keep -Xnocompile $CLUSTER_WSDL 
+cp $TMPDIR/$CLUSTER_PKGDIR/ClusterBean.java $DESTDIR/$CLUSTER_PKGDIR
+cp $TMPDIR/$CLUSTER_PKGDIR/ClusterBeanService.java $DESTDIR/$CLUSTER_PKGDIR
+cp $TMPDIR/$CLUSTER_PKGDIR/ObjectFactory.java $DESTDIR/$CLUSTER_PKGDIR
+cp $TMPDIR/$CLUSTER_PKGDIR/Update.java $DESTDIR/$CLUSTER_PKGDIR
+cp $TMPDIR/$CLUSTER_PKGDIR/UpdateResponse.java $DESTDIR/$CLUSTER_PKGDIR
+cp $TMPDIR/$CLUSTER_PKGDIR/UpdateResponse.java $DESTDIR/$CLUSTER_PKGDIR
+wsimport -verbose -s $TMPDIR -p $CLIENT_PACKAGE -keep -Xnocompile $CLIENT_WSDL
+cp $TMPDIR/$CLIENT_PKGDIR/ClientBean.java $DESTDIR/$CLIENT_PKGDIR
+cp $TMPDIR/$CLIENT_PKGDIR/ClientBeanService.java $DESTDIR/$CLIENT_PKGDIR
+cp $TMPDIR/$CLIENT_PKGDIR/ObjectFactory.java $DESTDIR/$CLIENT_PKGDIR
+cp $TMPDIR/$CLIENT_PKGDIR/RunGraph.java $DESTDIR/$CLIENT_PKGDIR
+cp $TMPDIR/$CLIENT_PKGDIR/RunGraphResponse.java $DESTDIR/$CLIENT_PKGDIR
 rm -r $TMPDIR
