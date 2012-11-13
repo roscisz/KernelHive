90ea461e5bf57325c645a87dd975c23ee30222ba
Paweł Rościszewski <pawel.rosciszewski@pg.edu.pl>
2012-11-13 18:17:35 +0000

    Bugfix in DeviceInfo.
    git-svn-id: https://kask.eti.pg.gda.pl/svn/kernelhive/KernelHive/src@458 1473d755-6451-4e3e-83a9-0691c74848a3

--- hive-jcommon/trunk/src/pl/gda/pg/eti/kernelhive/common/clientService/DeviceInfo.java
+++ hive-jcommon/trunk/src/pl/gda/pg/eti/kernelhive/common/clientService/DeviceInfo.java
@@ -3,6 +3,10 @@ package pl.gda.pg.eti.kernelhive.common.clientService;
 public class DeviceInfo {
 	
 	public String deviceString;
+	
+	public DeviceInfo() {
+		
+	}
 
 	public DeviceInfo(String deviceString) {
 		this.deviceString = deviceString;
