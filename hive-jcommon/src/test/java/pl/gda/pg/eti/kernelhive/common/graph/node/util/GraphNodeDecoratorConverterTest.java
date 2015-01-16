7c2d102227049925466230136f5ab58a9869514d
Piotr Dorożyński <piotr.doro.dorozynski@gmail.com>
2015-01-16 08:54:34 +0000

    hive-jcommon converted from Ant to Maven build.
    git-svn-id: https://kask.eti.pg.gda.pl/svn/kernelhive/KernelHive/src@545 1473d755-6451-4e3e-83a9-0691c74848a3

---
+++ hive-jcommon/src/test/java/pl/gda/pg/eti/kernelhive/common/graph/node/util/GraphNodeDecoratorConverterTest.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2014 Gdansk University of Technology
+ * Copyright (c) 2014 Marcel Schally-Kacprzak
+ *
+ * This file is part of KernelHive.
+ * KernelHive is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * KernelHive is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with KernelHive. If not, see <http://www.gnu.org/licenses/>.
+ */
+package pl.gda.pg.eti.kernelhive.common.graph.node.util;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import pl.gda.pg.eti.kernelhive.common.graph.builder.GraphNodeBuilderException;
+import pl.gda.pg.eti.kernelhive.common.graph.builder.impl.GraphNodeBuilder;
+import pl.gda.pg.eti.kernelhive.common.graph.node.EngineGraphNodeDecorator;
+import pl.gda.pg.eti.kernelhive.common.graph.node.GUIGraphNodeDecorator;
+import pl.gda.pg.eti.kernelhive.common.graph.node.GraphNodeType;
+import pl.gda.pg.eti.kernelhive.common.graph.node.IGraphNode;
+
+public class GraphNodeDecoratorConverterTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testConvertGuiToEngine() throws GraphNodeBuilderException, GraphNodeDecoratorConverterException {
+		IGraphNode node = new GraphNodeBuilder().setType(GraphNodeType.GENERIC).setId("test").build();
+		GUIGraphNodeDecorator guiNode = new GUIGraphNodeDecorator(node);
+		EngineGraphNodeDecorator engineNode = GraphNodeDecoratorConverter.convertGuiToEngine(guiNode);
+		assertEquals(guiNode.getGraphNode(), engineNode.getGraphNode());
+	}
+
+}
