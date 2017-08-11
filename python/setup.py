4df03b9c2b1331c9dca28a072e36f18b5cc7189a
Paweł Rościszewski <pawel.rosciszewski@pg.edu.pl>
2017-08-11 19:03:56 +0200

    add setup for PyPI distribution

---
+++ python/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'kernelhive',
+    version = '1.2.4',
+    packages = find_packages(),
+    description = 'Python libraries of KernelHive - an environment for executing, designing and tuning parallel applications in multi-level heterogeneous HPC systems',
+    author = 'Pawel Rosciszewski',
+    author_email = 'pawel.rosciszewski@pg.edu.pl',
+    url = 'https://github.com/roscisz/KernelHive',
+    download_url = 'https://github.com/roscisz/KernelHive/archive/1.2.4.tar.gz',
+    keywords = 'parallel cluster monitoring resource management',
+    install_requires=['paramiko', 'werkzeug', 'json-rpc']
+)
