f01e8cffc5eeabac2cc876f86b3f39ae5e04448f
Paweł Rościszewski <pawel.rosciszewski@pg.edu.pl>
2017-08-31 16:07:21 +0200

    version 1.2.6

--- python/setup.py
+++ python/setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'kernelhive',
-    version = '1.2.5',
+    version = '1.2.6',
     packages = find_packages(),
     description = 'Python libraries of KernelHive - an environment for executing, designing and tuning parallel applications in multi-level heterogeneous HPC systems',
     author = 'Pawel Rosciszewski',
     author_email = 'pawel.rosciszewski@pg.edu.pl',
     url = 'https://github.com/roscisz/KernelHive',
-    download_url = 'https://github.com/roscisz/KernelHive/archive/1.2.5.tar.gz',
+    download_url = 'https://github.com/roscisz/KernelHive/archive/1.2.6.tar.gz',
     keywords = 'parallel cluster monitoring resource management',
     install_requires=['paramiko', 'werkzeug', 'json-rpc']
 )
